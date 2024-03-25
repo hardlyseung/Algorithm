d5f14eb616d12b16e61880630b9c50113ccedfa0
hardlyseung <164846229+hardlyseung@users.noreply.github.com>
2024-03-25 16:48:02 +0900

    Update 16967.c

--- 16967.c
+++ 16967.c
@@ -1,33 +1,33 @@
 #include <iostream>
 using namespace std;
  
-int arrB[610][610];
+int arrC[610][610];
 int map[610][610];
  
 int H,W,X,Y;
  
 int main(){
     cin >> H >> W  >> X >> Y;
-    int rowB = H+X;
-    int colB = W+Y;
+    int rowC = H+X;
+    int colC = W+Y;
     
-    for(int i = 0; i<rowB; i++){
-        for(int j = 0; j<colB; j++){
-            scanf("%d", &arrB[i][j]);
+    for(int i = 0; i<rowC; i++){
+        for(int j = 0; j<colC; j++){
+            scanf("%d", &arrC[i][j]);
             map[i][j]= 0;
         }
     }
     
     for(int i = 0; i<H; i++){
         for(int j = 0; j<W; j++){
-            map[i][j] = arrB[i][j];
+            map[i][j] = arrC[i][j];
         }
     }
     
     
-    for(int row = X; row<=rowB; row++){
+    for(int row = X; row<=rowC; row++){
         bool chk=false;
-        for(int col = Y; col<=colB; col++){
+        for(int col = Y; col<=colC; col++){
             if(map[row][col] != 0){
                 map[row][col] -= map[row-X][col-Y];
                 chk = true;
